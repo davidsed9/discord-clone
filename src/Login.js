91db563abcf8e1c48a5337d426baf80571663725
davidsed9 <davidsed9@gmail.com>
2023-03-14 14:43:18 +0000

    first huge commit

---
+++ src/Login.js
@@ -0,0 +1,26 @@
+import { Button } from '@mui/material';
+import { auth, provider } from "./features/firebase";
+import React from 'react';
+import "./Login.css";
+
+function Login() {
+    const signIn = () => {
+        //do clever google login ....
+        auth.signInWithPopup(provider).catch((error) => alert(error.message));
+    };
+  return (
+    <div className='login'> 
+        <div className='login__logo'>
+            <img 
+            src="https://1000logos.net/wp-content/uploads/2021/06/Discord-logo-500x281.png" alt="" />
+        </div>
+
+
+    <Button onClick={signIn}>Sign In </Button>
+    </div>
+
+
+  )
+}
+
+export default Login
